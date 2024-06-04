5b8b4f855420a54544c0aa118f95f92b9685638e
VhahahaV <czq666@sjtu.edu.cn>
2024-06-05 00:17:03 +0800

    add Dependence

--- cmake-build-debug/3rdparty/rapidjson/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/czq/Project/3D_Modeling/RenderEngine/3rdparty/rapidjson
-# Build directory: /home/czq/Project/3D_Modeling/RenderEngine/cmake-build-debug/3rdparty/rapidjson
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("doc")
-subdirs("example")
-subdirs("test")
